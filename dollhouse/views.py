316b979cdf2b4a55b7e41a2c4ac83020f7a19468
littleeleventhwolf <2367785195@qq.com>
2016-10-03 18:14:22 +0800

    use django template

--- dollhouse/views.py
+++ dollhouse/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 # welcome message
 def welcome(request):
-	return HttpResponse("Welcome to DollHouse!")
+	return render_to_response('html/welcome.html')
