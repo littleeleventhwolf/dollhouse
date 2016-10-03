75cd96241252efd61077f5e60c520e73354224a6
littleeleventhwolf <2367785195@qq.com>
2016-10-03 19:09:53 +0800

    modify the template file

--- dollhouse/views.py
+++ dollhouse/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render_to_response
 
 # welcome message
 def welcome(request):
-	return render_to_response('html/welcome.html')
+	return render_to_response('welcome.html')
